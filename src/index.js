83a19c1b26b3f671e918d0f6e74506ed34945a50
jason-nordheim <jason.nordheim@gmail.com>
2020-07-23 15:22:50 -0600

    added route for app layout container

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { LandingPage } from './landing.page'
 import { BrowserRouter, Route } from 'react-router-dom'
+import { AppLayout } from './app.layout'
 
 
 const App = () => {
   return <div>
     <h1>Protected React Router</h1>
     <Route exact path="/" component={LandingPage} /> 
+    <Route path="/app" component={AppLayout} /> 
   </div>
 }
 
