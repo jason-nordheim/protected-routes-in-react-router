cea1c0bf389033bacddf9267a1b53e2797a16ada
jason-nordheim <jason.nordheim@gmail.com>
2020-07-23 15:17:41 -0600

    wrapped app component with browser router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { LandingPage } from './landing.page'
+import { BrowserRouter } from 'react-router-dom'
+
 
 const App = () => {
   return <div>
@@ -9,8 +11,8 @@ const App = () => {
 }
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </BrowserRouter>,
+  document.getElementById("root")
 );
